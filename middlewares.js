37cf5a4d1d863f9c4f93a47a62612481dbbddf77
viny <viny0698@gmail.com>
2023-01-18 18:56:30 +0530

    handle error in auth middleware

--- middlewares.js
+++ middlewares.js
@@ -1,27 +1,32 @@
 const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'].split(' ')[1].trim()
-
-  if (!token) {
-    return res.status(401).json({
-      code: 401,
-      message: 'unauthenticated',
-      data: {}
-    })
-  }
-
   try {
-    const { username } = jwt.verify(token, 'secret')
-    req.username = username
-    next()
-  } catch {
-    return res.status(401).json({
-      code: 401,
-      message: 'invalid jwt token',
-      data: {}
-    })
+    const token = req.headers['authorization'].split(' ')[1].trim()
+
+    if (!token) {
+      return res.status(401).json({
+        code: 401,
+        message: 'unauthenticated',
+        data: {}
+      })
+    }
+  
+    try {
+      const { username } = jwt.verify(token, 'secret')
+      req.username = username
+      next()
+    } catch {
+      return res.status(401).json({
+        code: 401,
+        message: 'invalid jwt token',
+        data: {}
+      })
+    }
+  } catch (err) {
+      next(err)
   }
+  
 }
 
 module.exports = { authMiddleware }
